efe06eb543f8287357f511b4950ef53947922883
N2C <nicolasdechastaing@gmail.com>
2018-01-09 10:03:50 +0100

    Update addPresident.php

--- addPresident.php
+++ addPresident.php
@@ -8,14 +8,51 @@
 $dbh = new PDO('mysql:host=localhost;dbname=app', "root", "");
 $query = $dbh->prepare("SELECT * FROM parti");
 $query->execute();
-$results = $query ->fetchAll();
+$partis = $query->fetchAll();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link type="text/css" rel="stylesheet" href="monCss.css" />
-</head>
-<body>
+<html>
+<head><link rel="stylesheet" type="text/css" href="https://educ.isep.fr/moodle/theme/standard/styles.php" />
+<link rel="stylesheet" type="text/css" href="https://educ.isep.fr/moodle/theme/ingenuous/styles.php" />
+
+	<meta charset="utf-8">
+	<title>Ajout d'un président</title>
+
+	</head>
+	<body>
+		<form method="post" action="addPresidentSubmit.php"><!-- Les valuers de method et action sont à compléter -->
+			<fieldset>
+				 <legend>Utilisateur</legend>
+                <label for="pseudo">Login</label><input name="pseudo" id="pseudo" type="text" /><br />
+              
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" /><br />
+			</fieldset>
+			<fieldset>
+				<legend>Président</legend>
+                    <label for="nom">Nom du président</label>
+                    <input type="text" name="nom" id="nom" /><br />
+
+                    <label for="prenom">Prénom du président</label>
+                    <input type="text" name="prenom" id="name" /><br />
+
+                    <label for"dateD">Date début mandat :</label>
+                    <input type="date" name="dateD" id="dateD" /><br />
+
+                    <label for"dateF">Date fin mandat :</label>
+                    <input type="date" name="dateF" id="dateF" /><br />
+
+
+				    <label for="id_parti">parti :</label>
+    				<select id="id_parti" name="id_parti">
+        				<?php foreach($partis as $parti){
+           				 $parti = $parti["intitule"];
+            			echo("<option value=\"$parti\">$parti</option>");
+						} ?>
+
+					</select>
+			</fieldset>
+			<input type="submit">
+		</form>
+	</body>
+</html>
